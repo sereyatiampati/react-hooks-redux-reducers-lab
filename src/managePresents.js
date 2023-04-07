a328f2c3a5327137a4d3385e853e6c0958265670
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-07 10:59:14 +0300

    done

--- src/managePresents.js
+++ src/managePresents.js
@@ -2,4 +2,12 @@ const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    case "presents/decrease":
+      return { numberOfPresents: state.numberOfPresents - 1 };
+    default:
+      return state;
+  }
 }
